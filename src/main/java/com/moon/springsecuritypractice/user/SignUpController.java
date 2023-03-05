4e49f09e6e24ac3d70339a9007401e4cbef4760d
MoonStar <moonsung0331@gmail.com>
2023-03-06 01:44:39 +0900

    #2 - 유저 회원가입 컨트롤러 누락된 어노테이션 추가

--- src/main/java/com/moon/springsecuritypractice/user/SignUpController.java
+++ src/main/java/com/moon/springsecuritypractice/user/SignUpController.java
@@ -1,14 +1,18 @@
 package com.moon.springsecuritypractice.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 회원가입 Controller
  */
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("signup")
 public class SignUpController {
 
     private final UserService userService;
