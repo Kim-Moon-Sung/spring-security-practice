e8e0ba7d71e98c028add6235fd9c828d8ce29216
MoonStar <moonsung0331@gmail.com>
2023-03-05 23:21:36 +0900

    스프링 시큐리티 프로젝트 초기화

---
+++ src/test/java/com/moon/springsecuritypractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
